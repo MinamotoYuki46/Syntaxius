242674df74473e957a90d0caa641de43eae14077
Minamoto Yuki <69187285+MhdFauzanAhsani@users.noreply.github.com>
2024-06-13 22:08:23 +0800

    versi yuki

---
+++ Gemastik/2021/A-UndianBerhadiah.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int32_t main()
+{
+    //c_p_c();
+    int n, x, minimum = INT_MAX;
+    cin >> n >> x;
+
+    vi coupon, result;
+
+    for(int i = 0; i < n; i++){
+        int insert;
+        cin >> insert;
+        coupon.pb(insert);
+    }
+
+    for(auto i: coupon){
+        minimum = min( abs(x - i), minimum);
+    }
+
+    for(auto j: coupon){
+        if(minimum == abs(x - j)){
+            result.pb(j);
+        }
+    }
+
+    sort(result.begin(), result.end());
+
+    for(auto k: result){
+        int zeros = 5 - trunc(log10(k) + 1);
+
+        while(zeros--){
+            cout << '0';
+        }
+        cout << k << endl;
+    }
+    return 0;
+}
